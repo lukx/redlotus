b6232944e2db5b98b4d4b316b847f1424fc874a3
penn5 <penn5@users.noreply.github.com>
2018-06-21 15:20:28 +0100

    Update downloadWithProgress.js
    enable md5 verification

--- src/helper/downloadWithProgress.js
+++ src/helper/downloadWithProgress.js
@@ -5,11 +5,11 @@ const url = require('url'),
     http = require('http');
     md5sum = require('./md5');
 
-module.exports = function mkdirAndDownload(fileUrl, destination) {
-    return mkdirpromise(path.dirname(destination)).then(() => download(fileUrl, destination));
+module.exports = function mkdirAndDownload(fileUrl, destination, md5correct) {
+    return mkdirpromise(path.dirname(destination)).then(() => download(fileUrl, destination, md5correct));
 };
 
-function download(fileUrl, destination) {
+function download(fileUrl, destination, md5correct) {
     return new Promise((resolve, reject) => {
 
         const p = url.parse(fileUrl);
@@ -33,8 +33,15 @@ function download(fileUrl, destination) {
                 downloadedPerc = newDlPerc;
             }).on('end', function () {
                 file.end();
-                console.log('md5 = '+md5.end());
+                md5val = md5.end()
+                console.log('md5 = '+md5val);
+                console.log('md5 correct = '+md5correct);
                 console.log('downloaded to: ' + destination);
+                if (md5val !== md5correct) {
+                    console.log('Error! MD5 does not match.');
+                    console.log(md5val);
+                    console.log(md5correct);
+                }
                 resolve(destination);
             }).on('error', function (err) {
                 reject(err);
